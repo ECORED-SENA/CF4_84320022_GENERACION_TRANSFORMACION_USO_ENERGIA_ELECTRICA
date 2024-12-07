351af1805a401c7573bfcf4404efb27e2230370c
Edwin Sneider Velandia Suarez <ivinernesto123@gmail.com>
2024-12-07 01:25:47 -0500

    avances

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -2,10 +2,153 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
+    .titulo-principal.color-acento-contenido(data-aos="flip-up")
       .titulo-principal__numero
         span 1
-      h1 Titulo de primer nivel
+      h1 Circuitos eléctricos
+    
+    .fondo-personalizado-1.f2.mb-3
+      .fondo-personalizado-1__contenido.pb-4
+        p.mb-4(data-aos="fade-up") Un circuito eléctrico es un conjunto de elementos conectados que permite el paso de energía eléctrica. Sus principales componentes incluyen resistencias, condensadores, y fuentes de tensión y corriente, representados mediante símbolos eléctricos.
+
+        .row.justify-content-center.mb-4(data-aos="fade-down")
+          .col-lg-8
+            .titulo-sexto.color-acento-contenido
+              p.mb-0 #[b Figura 1.] Componentes eléctricos y sus símbolos
+            .fondo-color-sistema-i.p-4
+              figure
+                img.mx-auto(src="@/assets/curso/temas/5.svg",alt="Tabla de componentes eléctricos con ejemplos de símbolos y unidades.")
+        
+        p.mb-0(data-aos="fade-right") Los circuitos eléctricos se componen de diversos elementos que cumplen funciones específicas para garantizar su correcto funcionamiento. A continuación, se presentan sus principales componentes y características:
+    
+    .fondo-personalizado-1.f3(data-aos="fade-up")
+      .fondo-personalizado-1__contenido.py-5
+        .bg-white.p-4.borde-20
+          SlyderA.slider-personalizado-1.pt-lg-3(tipo="b")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Generadores
+                p.mb-0 Generan energía eléctrica a partir de otras formas de energía. Ejemplo: pilas, baterías, dinamos y alternadores.
+              .col-lg-5
+                .borde-12.fondo-color-secundario.p-4
+                  figure
+                    img.mx-auto(src="@/assets/curso/temas/7.png",style="max-width: 417px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Receptores
+                p.mb-0 Consumen energía eléctrica y la transforman en calor, luz, movimiento o sonido. Ejemplo: lámparas, timbres, motores, resistencias y radiadores.
+              .col-lg-5
+                .borde-12.fondo-color-secundario.p-4
+                  figure
+                    img.mx-auto(src="@/assets/curso/temas/8.png",style="max-width: 417px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Elementos de maniobra
+                p.mb-4 Controlan la apertura o cierre de circuitos:
+                ul.lista-ul.mb-0.pb-0
+                  li.d-flex.mb-0
+                    i.fas.fa-check-circle.c-color-sistema-k.pe-2
+                    span #[b Interruptores:] abren o cierran un circuito de manera permanente.
+                  li.d-flex.mb-0
+                    i.fas.fa-check-circle.c-color-sistema-k.pe-2
+                    span #[b Pulsadores:] abren o cierran un circuito mientras están presionados, ya sean normalmente abiertos o cerrados.
+                  li.d-flex.mb-0
+                    i.fas.fa-check-circle.c-color-sistema-k.pe-2
+                    span #[b Conmutadores:] permiten controlar dos o más circuitos desde un mismo punto, como en el caso de lámparas con encendido desde ubicaciones diferentes.
+                    
+              .col-lg-5
+                .borde-12.fondo-color-secundario.p-4
+                  figure
+                    img.mx-auto(src="@/assets/curso/temas/9.png",style="max-width: 417px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Elementos de protección
+                p.mb-0 Protegen los circuitos contra cortocircuitos y sobrecargas. Ejemplo: fusibles, diferenciales y magnetotérmicos.
+                    
+              .col-lg-5
+                .borde-12.fondo-color-secundario.p-4
+                  figure
+                    img.mx-auto(src="@/assets/curso/temas/10.png",style="max-width: 417px;")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-5 Conductores
+                p.mb-0 Facilitan el paso de la corriente entre los elementos del circuito. Usualmente están hechos de cobre.
+                    
+              .col-lg-5
+                .borde-12.fondo-color-secundario.p-4
+                  figure
+                    img.mx-auto(src="@/assets/curso/temas/11.png",style="max-width: 417px;")
+    
+    .titulo-decorativo.my-5(data-aos="fade-right")
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Representación gráfica de los componentes
+    
+    .fondo-personalizado-1.f2.mb-4
+      .fondo-personalizado-1__contenido
+
+        p.mb-5(data-aos="fade-left") Cada elemento tiene su símbolo gráfico en los esquemas eléctricos, como interruptores, conmutadores, pulsadores, bombillas, baterías y motores.
+
+        .row.justify-content-center(data-aos="fade-right")
+          .col-lg-6
+            .titulo-sexto.color-acento-contenido
+              p.mb-0 #[b Figura 2.] Representación gráfica de los componentes
+            .fondo-color-sistema-i.p-4
+              figure
+                img.mx-auto(src="@/assets/curso/temas/12.svg",alt="Representación de elementos de control y salida: interruptor, conmutador, pulsador, bombilla, pila y motor.")
+        
+        .titulo-decorativo.my-5(data-aos="fade-right")
+          .cont-figura
+            img(src="@/assets/curso/temas/img-tit-dec.svg")
+          .cont-text 
+            h3 Conexión de los componentes de un circuito
+        
+        p.mb-0(data-aos="fade") En un circuito eléctrico, los componentes pueden conectarse de diversas maneras dependiendo de su propósito y diseño. A continuación, se describen las principales formas de conexión:
+    
+    .row.justify-content-center
+      .col-lg-4.col-md-6.mb-4(data-aos="fade-right")
+        .borde-23.fondo-color-sistema-j.p-4.h-100
+          figure.align-content-end.mb-4(style="min-height: 160px;")
+            img.mx-auto(src="@/assets/curso/temas/13.svg",style="max-width: 305px;")
+          
+          .text-center.mb-4
+            h4.mb-0.fondo-color-secundario.d-inline-flex.py-1.px-4 Circuito en serie
+          ul.lista-ul.mb-0.pb-0
+            li.d-flex.mb-3
+              i.fas.fa-check-circle.c-color-sistema-k.pe-2
+              span Los elementos están conectados uno tras otro, compartiendo la misma corriente.
+            li.d-flex.mb-0
+              i.fas.fa-check-circle.c-color-sistema-k.pe-2
+              span El voltaje total del circuito se distribuye entre los receptores.
+      .col-lg-4.col-md-6.mb-4(data-aos="fade")
+        .borde-23.fondo-color-sistema-j.p-4.h-100
+          figure.align-content-end.mb-4(style="min-height: 160px;")
+            img.mx-auto(src="@/assets/curso/temas/14.svg",style="max-width: 299px;")
+          
+          .text-center.mb-4
+            h4.mb-0.fondo-color-secundario.d-inline-flex.py-1.px-4 Circuito en paralelo
+          ul.lista-ul.mb-0.pb-0
+            li.d-flex.mb-3
+              i.fas.fa-check-circle.c-color-sistema-k.pe-2
+              span Los receptores están conectados de manera independiente entre dos puntos del circuito.
+            li.d-flex.mb-0
+              i.fas.fa-check-circle.c-color-sistema-k.pe-2
+              span El voltaje es igual en cada punto, pero la corriente varía según las características de los receptores.
+      .col-lg-4.col-md-6.mb-4(data-aos="fade-left")
+        .borde-23.fondo-color-sistema-j.p-4.h-100
+          figure.align-content-end.mb-4(style="min-height: 160px;")
+            img.mx-auto(src="@/assets/curso/temas/15.svg",style="max-width: 302px;")
+          
+          .text-center.mb-4
+            h4.mb-0.fondo-color-secundario.d-inline-flex.py-1.px-4 Circuito mixto
+          ul.lista-ul.mb-0.pb-0
+            li.d-flex.mb-3
+              i.fas.fa-check-circle.c-color-sistema-k.pe-2
+              span Combina conexiones en serie y en paralelo.
+            li.d-flex.mb-0
+              i.fas.fa-check-circle.c-color-sistema-k.pe-2
+              span Para resolver este tipo de circuitos, primero se analizan las conexiones en paralelo y luego las de serie.
       
 </template>
 
--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -2,10 +2,346 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
+    .titulo-principal.color-acento-contenido.mb-0
       .titulo-principal__numero
         span 2
-      h1 Titulo de primer nivel
+      h1 Ley de Ohm
+    
+    .fondo-personalizado-1.f2.pt-5.mb-5
+      .fondo-personalizado-1__contenido
+        p.mb-4 Esta ley, considerada fundamental para resolver matemáticamente circuitos, fue descubierta por el físico alemán Georg Simon Ohm, quien encontró la relación existente entre las tres magnitudes fundamentales de un circuito eléctrico: #[b el voltaje, la corriente y la resistencia], considerando esta relación en la siguiente fórmula:
+
+        .row.justify-content-center.mb-4
+          .col-lg-8
+            .borde-11.fondo-color-sistema-l.px-5.py-4
+              .row.align-items-center
+                .col-md-auto
+                  figure.mb-4.mb-md-0
+                    img.mx-auto(src="@/assets/curso/temas/16.svg",style="max-width: 87px;")
+                .col
+                  h4.mb-2.text-white Importante
+                  p.mb-0.text-white Cuando se resuelven problemas de la ley de Ohm se debe despejar cada una de las variables en función de cuál sea la incógnita preguntada.
+        
+        p.mb-4 El siguiente gráfico detalla estas operaciones. Oculte la variable que desea despejar y, entre las que permanezcan visibles, si están a la misma altura, colóquelas con un signo de multiplicar; si una está sobre otra, utilice un signo de dividir.
+
+        .row.justify-content-center
+          .col-lg-6
+            .titulo-sexto.color-acento-contenido
+              p.mb-0 #[b Figura 3.] Triángulos de Ohm
+            .fondo-color-sistema-i.p-4.py-lg-5
+              figure.mb-4.mb-lg-0
+                img.mx-auto(src="@/assets/curso/temas/17.svg",style="max-width: 490px;",alt="Representación visual de la Ley de Ohm con sus fórmulas básicas.")
+  
+        .titulo-decorativo.mt-5
+          .cont-figura
+            img(src="@/assets/curso/temas/img-tit-dec.svg")
+          .cont-text 
+            h3 Ejercicios prácticos
+    
+    p.fw-bold.mb-4 Ejemplo 1 
+    
+    p.mb-5 Un circuito eléctrico está formado por una pila de petaca de 4.5V, una bombilla que tiene una resistencia de 90Ω, un interruptor y los cables necesarios para unir todos ellos. Se pide una representación gráfica del circuito y que se calcule la intensidad de la corriente que circulará cada vez que cerremos el interruptor.
+
+    .row.justify-content-center.mb-4
+      .col-lg-6
+        .titulo-sexto.color-acento-contenido
+          p.mb-0 #[b Figura 4.] Circuito básico con lámpara y batería
+        .fondo-color-sistema-i.p-4.py-lg-5
+          figure
+            img.mx-auto(src="@/assets/curso/temas/18.svg",style="max-width: 511px;",alt="Circuito eléctrico simple compuesto por una batería como fuente de energía, un interruptor que controla el flujo de corriente y una bombilla que se ilumina cuando el circuito está cerrado.")
+    
+    .row
+      .col-lg-6.mb-4.borde-p-1
+        p.fw-bold.mb-4 Ejemplo 2
+        p.mb-5 En un circuito con una resistencia y una pila de 20V circula una corriente de 0.2A. Calcular el valor de dicha resistencia.
+        .fondo-color-sistema-i.p-5.d-inline-flex
+          figure
+            img(src="@/assets/curso/temas/19.svg",style="max-width: 176px;")
+      .col-lg-6.mb-4.ps-4
+        p.fw-bold.mb-4 Ejemplo 3
+        p.mb-5 ¿Cuál será la tensión que suministra una pila sabiendo que al conectarla a un circuito que tiene una resistencia de 45Ω, la intensidad es de 0.1A?
+        .fondo-color-sistema-i.p-5.d-inline-flex
+          figure
+            img(src="@/assets/curso/temas/20.svg",style="max-width: 204px;")
+    
+    .titulo-decorativo.mb-5
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Resolución de circuitos en serie
+
+    p.mb-5 Para calcular ya sea la corriente o el voltaje en un circuito con cargas en serie, primero se suman todas las cargas o resistencias para formar una resistencia total o equivalente y a partir de ahí calcular las demás variables mediante la ley de Ohm. Por lo tanto, la resistencia total de un circuito en serie se calcula de la siguiente forma:
+
+    .row.justify-content-center
+      .col-lg-10
+        .titulo-sexto.color-acento-contenido
+          p.mb-0 #[b Figura 5.] Circuito en serie con resistencias y fuente de 90V
+        .fondo-color-sistema-i.p-4
+          .row
+            .col-lg-5
+              figure.mb-4.mb-lg-0
+                img.mx-auto(src="@/assets/curso/temas/21.svg",style="max-width: 338px;")
+            .col-lg-7
+              p.fw-bold.mb-2 Ejemplo
+              p.mb-4 Calcular la corriente total que circula en el siguiente circuito con cargas en serie, considerando que la fuente es de 90 volts.
+              p.fw-bold.mb-2 Solución:
+              p.mb-2 #[b Paso 1:] se suman todas las resistencias para obtener la equivalente:
+              p.mb-3 #[em R total = 10Ω + 5Ω + 2Ω + 8Ω + 20Ω = 45Ω]
+              p.mb-3 #[b Paso 2:] como la incógnita es la corriente, se despeja I de la ecuación de la ley de Ohm y se sustituye:
+              math
+                mi I
+                mo =
+                mfrac
+                  mrow
+                    mi V
+                  mi R
+                mo =
+                mfrac
+                  mrow
+                    mn 90
+                    mi V
+                  mrow
+                    mn 45
+                    mo &#x2126;
+                mo =
+                mn 2
+                mi A
+    
+    .titulo-decorativo.my-5
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Fuentes de voltaje en serie
+    
+    p.mb-4 Las fuentes de voltaje también pueden colocarse en serie, por lo tanto, el voltaje total en un circuito donde existen dos o más fuentes en serie es la suma de los voltajes individuales de cada fuente. Cuando las polaridades de las fuentes se encuentran hacia la misma dirección, su voltaje se suma. Cuando sus polaridades se encuentran en direcciones opuestas, se restan.
+
+    .fondo-personalizado-1.f3(data-aos="fade-up")
+      .fondo-personalizado-1__contenido.py-5
+        .bg-white.p-4.borde-20
+          SlyderA.slider-personalizado-1.pt-lg-3(tipo="b")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-4 Ejemplo 1
+                p.mb-4 Para el siguiente circuito, calcular la corriente aportada por las dos fuentes en serie:
+                p.fw-bold.mb-0 Solución:
+                p.mb-3 #[b Paso 1:] se debe obtener el voltaje total del circuito, por lo cual se debe sumar o restar las fuentes de voltajes:
+                p.mb-3 𝐕 𝐭𝐨𝐭𝐚𝐥 = 12V + 5V = 17V
+                p.mb-3 #[b Paso 2:] una vez obtenido el voltaje total, se puede despejar I de la ecuación de la ley de Ohm y obtener la corriente total aportada por las dos fuentes:
+                math(style="font-weight: bold")
+                  mi I
+                  mo =
+                  mfrac
+                    mi V
+                    mi R
+                  mo =
+                  mfrac
+                    mrow
+                      mn 17
+                      mi V
+                    mrow
+                      mn 1
+                      mi K
+                      mo &#x2126;
+                  mo =
+                  mn 17
+                  mi m
+                  mi A
+              .col-lg-5
+                figure.pt-3
+                  img.mx-auto(src="@/assets/curso/temas/22.svg",style="max-width: 400px;",alt="Circuito con dos fuentes de 12 V y 5 V en serie y resistencia de 1.0 kΩ.")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-4 Ejemplo 2
+                p.mb-4 Obtener el valor de la resistencia del circuito para que circule una corriente de 2.5A si se tienen dos fuentes en serie con su valor respectivo:
+                p.fw-bold.mb-0 Solución:
+                p.mb-2 #[b Paso 1:] obtener el voltaje total:
+                p.mb-3 𝐕 𝐭𝐨𝐭𝐚𝐥 = 25V − 5V = 20V
+                p.mb-3 #[b Paso 2:] calcular la resistencia a partir de la ley de Ohm con los datos conocidos:
+                math(style="font-weight: bold")
+                  mi R
+                  mo =
+                  mfrac
+                    mi V
+                    mi I
+                  mo =
+                  mfrac
+                    mrow
+                      mn 20
+                      mi V
+                    mrow
+                      mn 2.5
+                      mi A
+                  mo =
+                  mn 8
+                  mo &#x2126;
+              .col-lg-5
+                figure.pt-3
+                  img.mx-auto(src="@/assets/curso/temas/23.svg",style="max-width: 400px;",alt="Circuito con fuentes de 25 V y 5 V en serie y resistencia desconocida.")
+            .row.justify-content-center.pt-2
+              .col-lg-6.mb-4.mb-lg-0
+                h4.mb-4 Ejemplo 3
+                p.mb-0 Calcular la corriente que circula por un circuito en serie que tiene una resistencia de carga de 1Ω y dos fuentes de voltaje directo dispuestas como se observa en el circuito mostrado:
+                p.fw-bold.mb-0 Solución:
+                p.mb-2 #[b Paso 1:] calcular el voltaje total del circuito. Para ello se observa la disposición de las fuentes; se puede ver que las dos son del mismo valor, sin embargo, los puntos en donde se unen son del mismo polo, por lo tanto, se están restando:
+                p.mb-2 𝐕 𝐭𝐨𝐭𝐚𝐥 = 6V − 6V = 0V
+                p.mb-3.fw-bold Sustituyendo:
+                math(style="font-weight: bold")
+                  mi I
+                  mo =
+                  mfrac
+                    mi V
+                    mi R
+                  mo =
+                  mfrac
+                    mrow
+                      mn 2
+                      mi V
+                    mrow
+                      mn 1
+                      mo &#x2126;
+                  mo =
+                  mn 0
+                  mi A
+              .col-lg-5
+                figure.pt-3
+                  img.mx-auto(src="@/assets/curso/temas/24.svg",style="max-width: 400px;",alt="Circuito con fuentes de 6 V y 6 V en serie y resistencia de 1.0 Ω.")
+
+    .titulo-decorativo.mt-5
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Resolución de circuitos en paralelo
+    
+    .fondo-personalizado-1.bottom-1.f2.mb-4
+      .fondo-personalizado-1__contenido.pt-5
+        p.mb-4 Para las resistencias en paralelo se pueden observar tres reglas principales para calcular la resistencia equivalente:
+
+        .row.justify-content-center
+          .col-lg-4.mb-4
+            .fondo-color-secundario.p-4.h-100
+              figure.mb-4
+                img.mx-auto(src="@/assets/curso/temas/25.svg",style="max-width: 77px;")
+              p.mb-0 Para un determinado número de resistencias en paralelo y del mismo valor, la resistencia total se calcula dividiendo el valor de una sola resistencia entre el número de ellas.
+          .col-lg-4.mb-4
+            .fondo-color-secundario.p-4.h-100
+              figure.mb-4
+                img.mx-auto(src="@/assets/curso/temas/26.svg",style="max-width: 77px;")
+              p.mb-0 La resistencia total de dos resistencias en paralelo de igual o distinto valor se puede calcular con la fórmula:
+              p.mb-0 Rt = (R1*R2) / (R1 + R2).
+        
+        p.mb-4.fw-bold Para calcular la resistencia equivalente de cualquier número de resistencias con diferentes o igual valor se usa la siguiente fórmula:
+
+        figure.mb-4
+          img.mx-auto(src="@/assets/curso/temas/27.svg",style="max-width: 400px;")
+        
+        p.fw-bold Ejemplo 1
+        p.mb-4 Encontrar la corriente que circula por el circuito mostrado, suponiendo que se tiene una fuente de 12V.
+
+        .row.justify-content-center
+          .col-lg-8
+            .titulo-sexto.color-acento-contenido
+              p.mb-0 #[b Figura 6.] Circuito en paralelo con resistencias y fuente de 12V
+            .fondo-color-sistema-i.p-4
+              figure
+                img.mx-auto(src="@/assets/curso/temas/28.svg",style="max-width: 584px;")
+    
+    p.fw-bold.mb-4 Solución
+
+    p.mb-4 Este ejemplo puede resolverse de dos maneras: calculando la corriente que circula por cada resistencia y sumándolas, o determinando la resistencia equivalente para obtener la corriente total. A continuación, se resolverá utilizando ambos métodos para comprobar que los resultados coinciden.
+
+    .row.justify-content-center.mb-4
+      .col-lg-10
+        .fondo-color-sistema-i.p-4.p-lg-5.mb-3
+          p.fw-bold.mb-4 Método 1. Calculando corrientes individuales
+          p.fw-bold.mb-0 Paso 1.
+          p.mb-4 En un circuito en paralelo el voltaje se mantiene constante entre cada división o rama, por lo que a partir del voltaje y resistencia se puede calcular la corriente que circula por cada rama mediante la ley de Ohm.
+          figure.mb-4
+            img.me-auto.d-none.d-lg-block(src="@/assets/curso/temas/29.svg",style="max-width: 470px;",alt="Fórmula Matemática")
+            img.me-auto.d-lg-none(src="@/assets/curso/temas/30.svg",style="max-width: 470px;",alt="Fórmula Matemática")
+          p.fw-bold.mb-0 Paso 2.
+          p.mb-4 Puesto que la corriente total es la suma de las corrientes individuales, se obtiene la corriente que circula en el circuito:
+          p.mb-1 I total = I1 + I2 + I3 + I4
+          p.mb-0 I total = 8 mA + 1.2 mA + 2.55 mA + 0.12 mA = 11.87 mA
+        .fondo-color-sistema-i.p-4.p-lg-5
+          p.fw-bold.mb-4 Método 2. Calculando la resistencia total
+          p.fw-bold.mb-0 Paso 1.
+          p.mb-4 Utilizar la suma de recíprocos, se calcula la resistencia total:
+          p.mb-4 [Ecuación]
+          p.fw-bold.mb-0 Paso 2.
+          p.mb-4 Ahora utilizando la ley de Ohm se calcula la corriente total:
+          p.mb-2 [Ecuación]
+          p puede identificar  utilizando los dos métodos se llega al mismo resultado.
+    
+    p.fw-bold.mb-4 Ejemplo 2
+    p.mb-4 Calcular el voltaje que proporciona la fuente para que exista una corriente de 6 amperes que fluye por todo el circuito de acuerdo al diagrama.
+
+    .row.justify-content-center.mb-3
+      .col-lg-8
+        .titulo-sexto.color-acento-contenido
+          p.mb-0 #[b Figura 7.] Circuito con resistencias en paralelo y fuentes desconocidas
+        .fondo-color-sistema-i.p-4
+          figure
+            img.mx-auto(src="@/assets/curso/temas/31.svg",style="max-width: 654px;",alt="Circuito mixto con resistencias de 3 kΩ en paralelo y 1 kΩ en serie, junto con una fuente desconocida.")
+    
+    .fondo-personalizado-1.bottom-2.f2.pt-3
+      .fondo-personalizado-1__contenido
+        .row.mb-5
+          .col-lg-6.borde-p-1
+            p.fw-bold.mb-4 Solución
+            p.fw-bold.mb-4 Paso 1.
+            p.mb-4 Calcular la resistencia equivalente. Revise que cada par de resistencias tiene un mismo valor. Por lo tanto, se puede aplicar la fórmula de producto/suma para calcular la resistencia equivalente de cada par o la fórmula para resistencias del mismo valor.
+            figure.mb-4.mb-lg-0
+              img.me-auto(src="@/assets/curso/temas/32.svg",style="max-width: 400px;",alt="Fórmula Matemática")
+          .col-lg-6.ps-lg-4
+            p.fw-bold.mb-4(style="opacity: 0;") .
+            p.fw-bold.mb-4 Paso 2.
+            p.mb-4 Calcular el par del lado izquierdo de la fuente:
+            figure.mb-4.mb-lg-0
+              img.me-auto(src="@/assets/curso/temas/33.svg",style="max-width: 210px;",alt="Fórmula Matemática")
+        
+        .row.align-items-center.mb-5
+          .col-lg-6
+            p.fw-bold.mb-4 Paso 3.
+            p.mb-4 Una vez que se tiene el circuito reducido a dos resistencias como se evidencia en el diagrama, se calcula la resistencia equivalente:
+            figure
+              img.me-auto(src="@/assets/curso/temas/34.svg",style="max-width: 505px;",alt="Fórmula Matemática")
+          .col-lg-6
+            figure
+              img.mx-auto(src="@/assets/curso/temas/35.png",style="max-width: 610px;")
+        
+        .row.justify-content-center
+          .col-lg-6
+            .titulo-sexto.color-acento-contenido
+              p.mb-0 #[b Figura 8.] Circuito reducido con resistencias en paralelo
+            .fondo-color-sistema-i.p-4
+              figure
+                img.mx-auto(src="@/assets/curso/temas/36.svg",style="max-width: 542px;",alt="Circuito paralelo con dos resistencias de 10 kΩ y 5 kΩ, y una fuente desconocida.")
+    
+    .fondo-personalizado-1.f2
+      .fondo-personalizado-1__contenido.py-5
+        p.fw-bold.mb-4 Paso 4.
+        p.mb-4 Una vez calculada la resistencia total, procedemos a obtener el voltaje de la fuente mediante la ley de Ohm:
+        figure.mb-4
+          img.me-auto(src="@/assets/curso/temas/37.svg",style="max-width: 295px;")
+        .row.justify-content-center
+          .col-lg-6
+            .titulo-sexto.color-acento-contenido
+              p.mb-0 #[b Figura 9.] Circuito equivalente con resistencia total y fuente de 1.99kV.
+            .fondo-color-sistema-i.p-4
+              figure
+                img.mx-auto(src="@/assets/curso/temas/38.svg",style="max-width: 542px;",alt="Circuito con una resistencia total de 333.33 Ω y una fuente de 1.99 kV.")
+    .titulo-decorativo.mb-5
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Resolución de circuitos mixtos
+
+
+          
+
+
+
+    
 
 </template>
 
